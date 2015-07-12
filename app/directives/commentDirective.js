cccc982eaf0611980e3538fdba10b7ee7e0ca660
Rondy Mesquita <rondinellimesquitas@gmail.com>
2015-07-12 00:39:09 -0300

    Intense Debate Support - testing

--- app/directives/commentDirective.js
+++ app/directives/commentDirective.js
@@ -3,14 +3,6 @@ angular.module('PhoenixCMS').directive('comments',['config', comments]);
 function comments(config){
     return {
         restrict: 'E',
-        templateUrl: 'app/themes/'+config.theme+'/comments/comments.html',
-        compile: function(element, attrs){
-            return function(scope, element, attrs){
-              // console.log(scope);
-              // scope.intenseDebateAcct = config.intenseDebateAcct;
-              // scope.intenseDebateId = "/post/"+scope.post.id+"/"+scope.post.url;
-              // scope.intenseDebateUrl = "/post/"+scope.post.id+"/"+scope.post.url;
-            }
-        }
+        templateUrl: 'app/themes/'+config.theme+'/comments/comments.html'
     };
 }
