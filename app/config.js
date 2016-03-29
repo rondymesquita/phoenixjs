731c5ffa4f558446265bde14c16728e2cb8a36dc
Rondy Mesquita <rondinellimesquitas@gmail.com>
2016-03-28 21:18:04 -0300

    Gulp -on going

--- app/config.js
+++ app/config.js
@@ -1,8 +1,8 @@
 
 phoenix.constant('config',{
-    theme: 'rising', /* theme folder name*/
-    intenseDebateAcct: '4fb72a3cc0a3dd8ee583e406d41ddafe', /*intenseDebate id*/
-    pagTemplate: 'app/themes/rising/partials/pagination.tpl.html', /*template for pagination*/
-    pagItemsPerPage: 2, /*how many itens per page*/
+    'theme': 'rising', /* theme folder name*/
+    'intenseDebateAcct': '4fb72a3cc0a3dd8ee583e406d41ddafe', /*intenseDebate id*/
+    'pagTemplate': 'app/themes/rising/partials/pagination.tpl.html', /*template for pagination*/
+    'pagItemsPerPage': 2 /*how many itens per page*/
     /*indexPage: 'custom_index.html',*/ /*if you want a custom index page, declare the page name here, otherwise, remove or comment the line*/
 });
--- gulpfile.js
+++ gulpfile.js
@@ -13,25 +13,28 @@ var fs = require('fs');
 var Q = require('q');
 var data = require('gulp-data');
 var runSequence = require('run-sequence');
-var jsonfile = require('jsonfile')
+var jsonfile = require('jsonfile');
+var cleanCSS = require('gulp-clean-css');
 
-// Definimos o diretorio dos arquivos para evitar repetição futuramente
-
-var files = [
+var phoenixJs = [
 	'./app/components/*.js',
 	'./app/*.js',
 	'./app/components/*/*.js'
 ];
 
-var libs = [
-	'bower_components/jquery/dist/jquery.js',
-	'bower_components/angular/angular.js',
-	'bower_components/angular-resource/angular-resource.js',
-	'bower_components/angular-route/angular-route.js',
-	'bower_components/angular-sanitize/angular-sanitize.js',
-	'bower_components/showdown/compressed/Showdown.js',
-	'bower_components/angular-markdown-directive/markdown.js',
-	'bower_components/angular-utils-pagination/dirPagination.js'
+var libraries = [
+	'./bower_components/jquery/dist/jquery.js',
+	'./bower_components/angular/angular.js',
+	'./bower_components/angular-resource/angular-resource.js',
+	'./bower_components/angular-route/angular-route.js',
+	'./bower_components/angular-sanitize/angular-sanitize.js',
+	'./bower_components/showdown/compressed/Showdown.js',
+	'./bower_components/angular-markdown-directive/markdown.js',
+	'./bower_components/angular-utils-pagination/dirPagination.js'
+];
+
+var contentJs = [
+	'./content/menus/menu.json'
 ];
 
 gulp.task('clean', function () {
@@ -45,50 +48,66 @@ gulp.task('cleanTempFiles', ['default'],function () {
 		.pipe(clean());
 });
 
-
-//Aqui criamos uma nova tarefa através do ´gulp.task´ e damos a ela o nome 'lint'
 gulp.task('lint', function() {
 
-	// Aqui carregamos os arquivos que a gente quer rodar as tarefas com o `gulp.src`
-	// E logo depois usamos o `pipe` para rodar a tarefa `jshint`
-	gulp.src(files)
-	.pipe(jshint())
-	.pipe(jshint.reporter('default'));
+	gulp
+		.src(phoenixJs)
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'));
 
 });
 
 gulp.task('js', function() {
 
 	gulp
-		.src(files)
+		.src(phoenixJs)
 		.pipe(concat('dist'))
 		.pipe(streamify(packer({base62: true, shrink: false})))
 		.pipe(rename('phoenix-min.js'))
 		.pipe(gulp.dest('dist'));
 
 	gulp
-		.src(libs)
+		.src(libraries)
 		.pipe(concat('libraries.js'))
 		//.pipe(rename('libraries.js'))
 		.pipe(minify())
 		.pipe(gulp.dest('dist'));
 
+	// contentJs.forEach(function(file){
+	// 	gulp
+	// 		.src(file)
+	// 		.pipe(minify())
+	// 		.pipe(gulp.dest('dist'));
+	// });
+
 });
 
 gulp.task('css', function() {
 
-	var content = fs.readFileSync('./app/config.js','utf8');
+	var theme = getThemeNameFromConfig();
+	try{
+		var stylesFile = fs.readFileSync('./app/themes/' + theme + '/styles.json','utf8');
 
-	var re = /\{((.*\s*)*)\}/; //capturing all between brackets
-	var regex = new RegExp(re);
-	var config = regex.exec(content).toString();
-	config = config.replace(/\/\*(.*)\*\//g,''); // removing comments
-	console.log("==="+config+"===");
-	JSON.parse(config);
-	// var theme = new RegExp(/theme: *\'(\w+)\'/).exec(config).toString();
+		var styles = [];
+		JSON.parse(stylesFile).forEach(function(style){
+			styles.push('app/themes/' + theme + "/" + style);
+		});
 
+		gulp
+			.src(styles)
+			.pipe(concat('dist'))
+			.pipe(cleanCSS({compatibility: 'ie8'}))
+			.pipe(rename('styles.css'))
+			.pipe(gulp.dest('dist'));
 
+	}catch(err){
+		console.log('Expected styles.json file does not exists. Theme styles will not be minified.');
+	}
 
+});
+
+gulp.task('test', function() {
+	getThemeNameFromConfig();
 });
 
 gulp.task('copy', function() {
@@ -105,24 +124,37 @@ gulp.task('copy', function() {
 	gulp
 		.src('app/themes/**')
 		.pipe(gulp.dest('dist/app/themes'));
-
-	gulp
-		.src('bower_components/angular-utils-pagination/dirPagination.tpl.html')
-		.pipe(gulp.dest('dist/modules'));
 });
 
 
-gulp.task('default', ['lint','copy','js'],function() {
+gulp.task('default', ['lint','copy','js','css'],function() {
 
 	//gulp.run('lint', 'copy', 'js'); //deprecated
 
 	// gulp.watch([files,libs], function(evt) {
 	// 	gulp.run('copy', 'js', 'js-libs'); //deprecated
 	// });
-	console.log('done');
 });
 
 gulp.task('hello', function() {
-  // place code for your default task here
   console.log("Hello!!!");
 });
+
+gulp.doneCallback = function(){
+
+}
+
+/* Functions */
+function getThemeNameFromConfig(){
+	var content = fs.readFileSync('./app/config.js','utf8');
+	content = content.replace(/(\/\*.*\*\/)/g,''); // removing comments
+	content = content.replace(/'/g, '"');
+	var regex = new RegExp(/(\{[\s\S]*\})/);//capturing all between brackets
+	var configString = regex.exec(content)[1];
+	var config = JSON.parse(configString)
+	return config.theme;
+}
+
+function logInfo(task, msg){
+	console.log('===> [Phoenix]['+task+'] '+ msg);
+}
